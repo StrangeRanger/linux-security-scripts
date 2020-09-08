b74c8cc44c8663040063119572a18a53376a5e48
StrangeRanger <strangeranger01@gmail.com>
2020-09-08 16:46:08 -0700

    build: add scripts for hardening Linux

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# linux-security-scripts
+# Linux Security Scripts
+
+[![Project Status: Active – The project has reached a stable, usable state and is being actively developed.](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active)
+
+This repository is a collection of scripts designed to secure/harden Linux based distributions.
+
+## Officially Supported Distributions
+
+Below is a list of Linux Distributions that the security scripts have been tested and confirmed to work on.
+
+| Distributions | Distro Versions |
+|---------------|-----------------|
+| Ubuntu        | 18.04           |
---
+++ hardening/lynis-installer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+################################################################################
+#
+# Lynis Installer
+# ===============
+# A script for installing and running lynis with recommended options
+#
+# Version: 1.0.0
+# License: MIT License
+#     Copyright (c) 2020 Hunter T.
+#
+################################################################################
+#
+    cyan=$'\033[0;36m'
+    red=$'\033[1;31m'
+    nc=$'\033[0m'
+
+    # Checks to see if this script was executed with root privilege
+    if [[ $EUID -ne 0 ]]; then 
+        echo "${red}Please run this script as or with root privilege${nc}"
+        echo -e "\nExiting..."
+        exit 1
+    fi
+
+    echo "Changing working directory to ~/${USER_SUDO}..."
+    cd /home/"$SUDO_USER" || {
+        echo "${red}Failed to change working directory to ~/${SUDO_USER}"
+        echo "${cyan}Lynis will download to ${PWD}${nc}"
+    }
+
+    echo "Downloading lynis..."
+    git clone https://github.com/CISOfy/lynis
+    echo "Changing ownership of lynis to root:root..."
+    chown -R root:root lynis
+    echo -e "${cyan}To peform a system scan with lynis, execute the following" \
+        "command in the lynis root directory: sudo ./lynis audit system"
