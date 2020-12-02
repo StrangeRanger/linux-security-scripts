01aacbd3bbd700adeb2a137f086a023e301c69f9
StrangeRanger <strangeranger01@randomserver.xyz>
2020-12-01 18:10:43 -0800

    style: change commenting style

--- hardening/root-locker.sh
+++ hardening/root-locker.sh
@@ -14,36 +14,41 @@
 #
 # [ Variables ]
 #
-################################################################################
-#
+###
     config_file_bak="/etc/shadow.bak"
     config_file="/etc/shadow"
     green=$'\033[0;32m'
     cyan=$'\033[0;36m'
     red=$'\033[1;31m'
     nc=$'\033[0m'
+###
+#
+# End of [ Variables ]
+################################################################################
 
-#
+
 ################################################################################
 #
-# [ Pre-main ]
+# [ Prepping ]
 #
-################################################################################
-#
+###
     # Checks to see if this script was executed with root privilege
     if ((EUID != 0)); then 
         echo "${red}Please run this script as or with root privilege${nc}" >&2
         echo -e "\nExiting..."
         exit 1
     fi
-
+###
 #
+# End of [ Prepping ]
+################################################################################
+
+
 ################################################################################
 #
 # [ Main ]
 #
-################################################################################
-#
+###
     read -p "We will now disable the root account. Press [Enter] to continue."
 
     # Only backs up the original shadow file
@@ -60,3 +65,8 @@
     echo "Disabling root account..."
     passwd -dl root && echo -e "\n${green}The root account has been locked${nc}" || 
         echo -e "\n${red}Failed to lock the root account${nc}"
+###
+#
+# End of [ Main ]
+################################################################################
+
