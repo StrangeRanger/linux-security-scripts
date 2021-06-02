4b3e5837eb6a963fd43179bc1119a6931b2e0013
Hunter T <strangeranger01@randomserver.xyz>
2021-06-02 11:33:40 -0700

    style(comments): update and modify style of comments

--- auditing/lynis-installer.sh
+++ auditing/lynis-installer.sh
@@ -4,9 +4,9 @@
 #
 # Version: v1.0.3
 # License: MIT License
-#   Copyright (c) 2020-2021 Hunter T.
+#          Copyright (c) 2020-2021 Hunter T.
 #
-################################################################################
+########################################################################################
 #### [ Variables ]
 
 
@@ -17,11 +17,11 @@ nc=$'\033[0m'
 
 
 ##### End of [ Variables ]
-################################################################################
+########################################################################################
 #### [ Prepping ]
 
 
-# Checks to see if this script was executed with root privilege
+## Check if the script was executed with root privilege.
 if [[ $EUID != 0 ]]; then
     echo "${red}Please run this script as or with root privilege${nc}" >&2
     echo -e "\nExiting..."
@@ -30,21 +30,21 @@ fi
 
 
 #### End of [ Prepping ]
-################################################################################
+########################################################################################
 #### [ Main ]
 
 
 read -rp "We will now download lynis. Press [Enter] to continue."
 
-echo "Changing working directory to '/home/${SUDO_USER}'..."
+echo "Changing working directory to '/home/$SUDO_USER'..."
 cd /home/"$SUDO_USER" || {
-    echo "${red}Failed to change working directory to '/home/${SUDO_USER}'"
-    echo "${cyan}Lynis will download to '${PWD}'${nc}"
+    echo "${red}Failed to change working directory to '/home/$SUDO_USER'"
+    echo "${cyan}Lynis will download to '$PWD'$nc"
 }
 
 echo "Downloading lynis..."
 git clone https://github.com/CISOfy/lynis || {
-    echo "${red}Failed to download lynis${nc}" >&2
+    echo "${red}Failed to download lynis$nc" >&2
     echo -e "\nExiting..."
     exit 1
 }
@@ -52,9 +52,9 @@ echo "Changing ownership of lynis to root:root..."
 chown -R root:root lynis
 
 echo -e "\n${green}Lynis has been downloaded to your system"
-echo -e "${cyan}To perform a system scan with lynis, execute the following" \
-    "command in the lynis root directory: sudo ./lynis audit system${nc}"
+echo -e "${cyan}To perform a system scan with lynis, execute the following command in" \
+    "the lynis root directory: sudo ./lynis audit system$nc"
 
 
 #### End of [ Main ]
-################################################################################
+########################################################################################
--- hardening/root-locker.sh
+++ hardening/root-locker.sh
@@ -4,9 +4,9 @@
 #
 # Version: v1.0.3
 # License: MIT License
-#   Copyright (c) 2020-2021 Hunter T.
+#          Copyright (c) 2020-2021 Hunter T.
 #
-################################################################################
+########################################################################################
 #### [ Variables ]
 
 
@@ -19,40 +19,39 @@ nc=$'\033[0m'
 
 
 ##### End of [ Variables ]
-################################################################################
+########################################################################################
 #### [ Prepping ]
 
 
-# Checks to see if this script was executed with root privilege
+## Check if this script was executed with root privilege.
 if [[ $EUID != 0 ]]; then
-    echo "${red}Please run this script as or with root privilege${nc}" >&2
+    echo "${red}Please run this script as or with root privilege$nc" >&2
     echo -e "\nExiting..."
     exit 1
 fi
 
 
 #### End of [ Prepping ]
-################################################################################
+########################################################################################
 #### [ Main ]
 
 
 read -rp "We will now disable the root account. Press [Enter] to continue."
 
-# Only backs up the original shadow file
+## Backup 'shadow' if 'shadow.bak' doesn't already exist.
 if [[ ! -f $config_file_bak ]]; then
     echo "Backing up original 'shadow'..."
-    cp $config_file $config_file_bak || {
+    cp "$config_file" "$config_file_bak" || {
         echo "${red}Failed to back up shadow" >&2
-        echo "${cyan}Please create a backup of the original 'shadow'" \
-            "before continuing${nc}"
+        echo "${cyan}Please create a backup of the original 'shadow' before continuing$nc"
         exit 1
     }
 fi
 
 echo "Disabling root account..."
-passwd -dl root && echo -e "\n${green}The root account has been locked${nc}" ||
-    echo -e "\n${red}Failed to lock the root account${nc}"
+passwd -dl root && echo -e "\n${green}The root account has been locked$nc" \
+    || echo -e "\n${red}Failed to lock the root account$nc"
 
 
 #### End of [ Main ]
-################################################################################
+########################################################################################
