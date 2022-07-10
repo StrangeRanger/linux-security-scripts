25d3798efb5440f86503c0356040e02c862c89f3
Hunter T <strangeranger01@randomserver.xyz>
2022-07-10 19:01:49 +0200

    func: refer to info below - Modified exit codes - No longer backs up /etc/shadow

--- hardening/root-locker.sh
+++ hardening/root-locker.sh
@@ -2,18 +2,15 @@
 #
 # Locks the root account and erases it's current password.
 #
-# Version: v1.0.3
+# Version: v1.0.4
 # License: MIT License
-#          Copyright (c) 2020-2021 Hunter T.
+#          Copyright (c) 2020-2022 Hunter T. (StrangeRanger)
 #
 ########################################################################################
 #### [ Variables ]
 
 
-config_file_bak="/etc/shadow.bak"
-config_file="/etc/shadow"
 green=$'\033[0;32m'
-cyan=$'\033[0;36m'
 red=$'\033[1;31m'
 nc=$'\033[0m'
 
@@ -27,7 +24,7 @@ nc=$'\033[0m'
 if [[ $EUID != 0 ]]; then
     echo "${red}Please run this script as or with root privilege$nc" >&2
     echo -e "\nExiting..."
-    exit 1
+    exit 2
 fi
 
 
@@ -38,19 +35,13 @@ fi
 
 read -rp "We will now disable the root account. Press [Enter] to continue."
 
-## Backup 'shadow' if 'shadow.bak' doesn't already exist.
-if [[ ! -f $config_file_bak ]]; then
-    echo "Backing up original 'shadow'..."
-    cp "$config_file" "$config_file_bak" || {
-        echo "${red}Failed to back up shadow" >&2
-        echo "${cyan}Please create a backup of the original 'shadow' before continuing$nc"
-        exit 1
-    }
-fi
-
 echo "Disabling root account..."
-passwd -dl root && echo -e "\n${green}The root account has been locked$nc" \
-    || echo -e "\n${red}Failed to lock the root account$nc"
+passwd -dl root || {
+    echo -e "\n${red}Failed to lock the root account$nc"
+    exit 1
+}
+
+echo -e "\n${green}The root account has been locked$nc"
 
 
 #### End of [ Main ]
