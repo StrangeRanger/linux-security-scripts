00edd71899f8f674279194b04640be73caa78f7e
Hunter T. <strangeranger@hthompson.dev>
2024-12-20 16:45:12 -0800

    style: modify style and colorization of output

--- hardening/Root Locker/root-locker.bash
+++ hardening/Root Locker/root-locker.bash
@@ -6,32 +6,39 @@
 #   Locking the root account doesn't prevent users from using something like `sudo su`
 #   to gain root access.
 #
-# Version: v1.0.7
+# Version: v1.0.8
 # License: MIT License
 #          Copyright (c) 2020-2024 Hunter T. (StrangeRanger)
 #
 ########################################################################################
 
 C_GREEN="$(printf '\033[0;32m')"
+C_BLUE="$(printf '\033[0;34m')"
+C_CYAN="$(printf '\033[0;36m')"
 C_RED="$(printf '\033[1;31m')"
 C_NC="$(printf '\033[0m')"
+
+C_SUCCESS="${C_GREEN}==>${C_NC} "
+C_ERROR="${C_RED}ERROR:${C_NC} "
+C_INFO="${C_BLUE}==>${C_NC} "
+C_NOTE="${C_CYAN}==>${C_NC} "
 
 
 ## Check if this script was executed with root privilege.
 if [[ $EUID != 0 ]]; then
-    echo "${C_RED}Please run this script as or with root privilege${C_NC}" >&2
-    echo -e "\nExiting..."
+    echo "${C_ERROR}Please run this script as or with root privilege" >&2
+    echo -e "\n${C_INFO}Exiting..."
     exit 1
 fi
 
 
-read -rp "We will now disable the root account. Press [Enter] to continue."
+read -rp "${C_NOTE}We will now disable the root account. Press [Enter] to continue."
 
-echo "Disabling root account..."
+echo "${C_INFO}Disabling root account..."
 usermod -L root || {
-    echo -e "${C_RED}ERROR:${C_NC} Failed to lock the root account" >&2
-    echo -e "\nExiting..."
+    echo -e "${C_ERROR}Failed to lock the root account" >&2
+    echo -e "\n${C_INFO}Exiting..."
     exit 1
 }
 
-echo -e "\n${C_GREEN}The root account has been locked${C_NC}"
+echo -e "\n${C_SUCCESS}The root account has been locked"
