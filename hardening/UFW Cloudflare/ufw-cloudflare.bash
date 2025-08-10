902dbcd393caf43d387b428bb926c9686f8038cb
Hunter T. <strangeranger@hthompson.dev>
2025-08-10 12:05:53 -0700

    chore: update version number

--- hardening/UFW Cloudflare/ufw-cloudflare.bash
+++ hardening/UFW Cloudflare/ufw-cloudflare.bash
@@ -2,7 +2,7 @@
 #
 # Set up UFW to only allow HTTP and HTTPS traffic from Cloudflare's IP ranges.
 #
-# Version: v1.0.0
+# Version: v1.0.1
 # License: MIT License
 #          Copyright (c) 2024-2025 Hunter T. (StrangeRanger)
 #
