d4b8e46ce6ca9bc619cd6e24bd0aef944d760302
Hunter T. <strangeranger@hthompson.dev>
2026-05-17 20:17:59 -0700

    chore(scripts): standardize color and status constants

--- hardening/UFW Cloudflare/ufw-cloudflare.bash
+++ hardening/UFW Cloudflare/ufw-cloudflare.bash
@@ -19,17 +19,16 @@ readonly C_SLEEP_TIME=1
 readonly C_CLOUDFLARE_IPV4_RANGES_URL="https://www.cloudflare.com/ips-v4/"
 readonly C_CLOUDFLARE_IPV6_RANGES_URL="https://www.cloudflare.com/ips-v6/"
 
-C_YELLOW="$(printf '\033[1;33m')"
-C_GREEN="$(printf '\033[0;32m')"
-C_BLUE="$(printf '\033[0;34m')"
-C_CYAN="$(printf '\033[0;36m')"
-C_RED="$(printf '\033[1;31m')"
-C_NC="$(printf '\033[0m')"
-readonly C_YELLOW C_GREEN C_BLUE C_CYAN C_RED C_NC
+readonly C_YELLOW=$'\033[1;33m'
+readonly C_GREEN=$'\033[0;32m'
+readonly C_BLUE=$'\033[0;34m'
+readonly C_CYAN=$'\033[0;36m'
+readonly C_RED=$'\033[1;31m'
+readonly C_NC=$'\033[0m'
 
-readonly C_SUCCESS="${C_GREEN}==>${C_NC} "
-readonly C_WARNING="${C_YELLOW}==>${C_NC} "
 readonly C_ERROR="${C_RED}ERROR:${C_NC} "
+readonly C_SUCC="${C_GREEN}==>${C_NC} "
+readonly C_WARN="${C_YELLOW}==>${C_NC} "
 readonly C_INFO="${C_BLUE}==>${C_NC} "
 readonly C_NOTE="${C_CYAN}==>${C_NC} "
 
@@ -53,15 +52,15 @@ clean_exit() {
 
     case "$exit_code" in
         0|1) echo "" ;;
-        129) echo -e "\n\n${C_WARNING}Hangup signal detected (SIGHUP)" ;;
-        130) echo -e "\n\n${C_WARNING}User interrupt detected (SIGINT)" ;;
-        143) echo -e "\n\n${C_WARNING}Termination signal detected (SIGTERM)" ;;
-        *)   echo -e "\n\n${C_WARNING}Exiting with code: $exit_code" ;;
+        129) echo -e "\n\n${C_WARN}Hangup signal detected (SIGHUP)" ;;
+        130) echo -e "\n\n${C_WARN}User interrupt detected (SIGINT)" ;;
+        143) echo -e "\n\n${C_WARN}Termination signal detected (SIGTERM)" ;;
+        *)   echo -e "\n\n${C_WARN}Exiting with code: $exit_code" ;;
     esac
 
     case $stage in
         2|3|4)
-            echo "${C_WARNING}Interrupt occurred during stage '$stage'; incomplete changes"
+            echo "${C_WARN}Interrupt occurred during stage '$stage'; incomplete changes"
             echo "${C_INFO}Temporarily disabling UFW..."
             ufw disable
             echo "${C_INFO}Restoring previous UFW rules..."
@@ -207,5 +206,5 @@ fi
 
 sleep "$C_SLEEP_TIME"
 echo "${C_NOTE}Waiting '$C_SLEEP_TIME' second for changes to take effect..."
-echo "${C_SUCCESS}Finished setting up UFW with Cloudflare IP ranges"
+echo "${C_SUCC}Finished setting up UFW with Cloudflare IP ranges"
 clean_exit 0
